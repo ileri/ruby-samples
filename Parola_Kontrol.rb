b559fe3c88f19f4eff554a8a32b0e46087a405fa
Serhat Celil İLERİ <celil.ileri@bil.omu.edu.tr>
2015-11-24 23:55:06 +0300

    Parola Kontrolü

---
+++ Parola_Kontrol.rb
@@ -0,0 +1,17 @@
+#encoding : UTF-8
+def sorgu (kadi,pass)
+	ka = "kullaniciadi"
+	password="123456"
+	if kadi == ka && pass == password
+		puts "Giriş Başarılı"
+	elsif kadi == ka && pass != password
+		puts "Kullanıcı Adı ile şifre uyuşmuyor"
+	else
+		puts "Böyle bir kullanıcı yok"
+	end
+end
+print "Kullanıcı adınızı giriniz : "
+k_adi = gets.chomp
+print "Parolanızı giriniz : "
+parola = gets.chomp
+sorgu(k_adi,parola)
