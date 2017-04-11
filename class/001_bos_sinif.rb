e3c9e4e0cb76a0d764f106ffe0b295d8915edc21
celilileri <celil.ileri@bil.omu.edu.tr>
2017-04-12 00:55:09 +0300

    Boş sınıf oluşturma kodu ekle

---
+++ class/001_bos_sinif.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+# Boş bir sınıf tanımı. Geçerlidir.
+# Ruby geleneğinde sınıf tanımlarken üstünde sınıfı kısaca tanıtan
+# bir yorum satırı olur. Şunun gibi:
+
+# Ruby'de sınıf oluşturma örneği için oluşturulan sınıf
+class Sinif
+end
