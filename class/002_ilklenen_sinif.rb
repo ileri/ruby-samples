a060f5e560bc42f2393f40cfeb8764ddad7a8fda
celilileri <celil.ileri@bil.omu.edu.tr>
2017-04-12 00:55:30 +0300

    Sınıf ilkleme kodu ekle

---
+++ class/002_ilklenen_sinif.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# Sınıfların ilkleme metodu initialize metodudur.
+class Sinif
+  # Sınıf oluşturulurken new isimli sınıf değişkeni initialize isimli
+  # nesne değişkenini çağırır 
+  def initialize
+  end
+end
+
+# Bir sınıftan yeni nesne türetmek için 
+# SinifAdi.new argumanlar
+# söz dizimini kullanırız.
+nesne = Sinif.new
