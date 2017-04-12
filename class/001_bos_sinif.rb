514e03ef6e2173afd49f3a96f4036045a8abbb2e
celilileri <celil.ileri@bil.omu.edu.tr>
2017-04-12 10:51:12 +0300

    SHEBANG tanımlaması ve boş sınıf açıklamasını sınıfın içine ekle

--- class/001_bos_sinif.rb
+++ class/001_bos_sinif.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
 
-# Boş bir sınıf tanımı. Geçerlidir.
-# Ruby geleneğinde sınıf tanımlarken üstünde sınıfı kısaca tanıtan
-# bir yorum satırı olur. Şunun gibi:
+# Yukarıdaki #! ile başlayan satır bir yorum satırı değildir.
+# Unix mimarisiyle tasarlanan işletim sistemlerinde çalıştırılabilir olan
+# dosyalar sistem tarafından hangi program veya yorumlayıcı ile çalıştırılacağı
+# belirtilmeden direkt olarak ./dosya_adi yada /dosyanin/tam/yolu şelinde
+# çalıştırıldığında sistem bu dosyayı çalıştırmak için dosyanın ilk satırında
+# yukarıda yazdığımız gibi bir "SHEBANG" arar.
+
+# SHEBANG dediğimiz şey sadece dosya içindeki bir satırdan ibarettir.
+
+# Kare, diez, hash yada sharp olarak adlandırılan # karakteri ve ünlem işareti
+# dediğimiz ! karakterinin bitişik olarak yazılıp ardından hemen boşluk
+# bırakmadan çalıştıracak programın tam yolunu yazarak SHEBANG oluştururuz.
+
 
 # Ruby'de sınıf oluşturma örneği için oluşturulan sınıf
 class Sinif
+  # Boş bir sınıf tanımı. Geçerlidir.
+  # Ruby geleneğinde sınıf tanımlarken üstünde sınıfı kısaca tanıtan
+  # bir yorum satırı olur. Şunun gibi:
 end
