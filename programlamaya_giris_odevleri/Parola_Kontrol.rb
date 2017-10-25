dd59feb377e6f12bb83e0cd80f6b0effbc86e50b
Serhat Celil İLERİ <celil.ileri@bil.omu.edu.tr>
2017-10-25 20:08:44 +0300

    kötü örnek olma , girintilemeyi 2 boşlukla yap

--- programlamaya_giris_odevleri/Parola_Kontrol.rb
+++ programlamaya_giris_odevleri/Parola_Kontrol.rb
@@ -1,14 +1,14 @@
 #encoding : UTF-8
 def sorgu (kadi,pass)
-	ka = "kullaniciadi"
-	password="123456"
-	if kadi == ka && pass == password
-		puts "Giriş Başarılı"
-	elsif kadi == ka && pass != password
-		puts "Kullanıcı Adı ile parola uyuşmuyor"
-	else
-		puts "Böyle bir kullanıcı yok"
-	end
+  ka = "kullaniciadi"
+  password="123456"
+  if kadi == ka && pass == password
+    puts "Giriş Başarılı"
+  elsif kadi == ka && pass != password
+    puts "Kullanıcı Adı ile parola uyuşmuyor"
+  else
+    puts "Böyle bir kullanıcı yok"
+  end
 end
 print "Kullanıcı adınızı giriniz : "
 k_adi = gets.chomp
